48e344ff8b666f6f5c335fdd09bd718c94eadb69
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-01-23 16:01:47 -0500

    Selecciona múltiples elementos mezclando métodos ('querySelector', 'getElementsByClassName')

--- js/app.js
+++ js/app.js
@@ -1,30 +1,15 @@
-/* Seleccionando múltiples elementos de JavaScript */
-const enlaces = document .getElementsByClassName( 'enlace' );           // Solo el nombre de la clase 'enlace'
-let enlace, enlacesArr; 
+/* Seleccionando múltiples elementos de JavaScript (Mezclando métodos de selección) */
+const listaEnlaces = document .querySelector( '#principal' ) .getElementsByClassName( 'enlace' );           
+let enlaces;
 
-/* Obtenemos un elemento de la colección de elementos*/ 
-/* FORMA 1 */ 
-enlace = enlaces[ 4 ] ;  
-console .log( 'enlaces[4] ', enlace .textContent );             // Temas
-enlace .style .textTransform = 'uppercase';
-enlace .style .fontWeight = 'bold';
-enlace .style .textDecoration = 'underline';
+/* Convertir Colección a 'Array' */
+enlaces = Array .from( listaEnlaces );
 
-console .log( enlaces );
-
-/* Convierte una Colección a un Array */
-enlacesArr = Array .from( enlaces );
-
-/* Recorre el array, aplica nuevos estilos a cada enlace */
-enlacesArr .forEach(element => {
-    element .style .color = '#999999';
-    element .style .fontSize = '18px';
+/* Recorre y cambia estilos de los elementos seleccionados */
+enlaces .forEach( function( elemento ) {
+    elemento .style .fontSize = '17px';
+    elemento .style .textDecoration = 'underline';
+    elemento .style .fontWeight = 'bold';
 });
 
-/* FORMA 2 */
-enlace = document .getElementsByClassName( 'enlace' )[ 4 ];     // Temas
-console .log( 'document.getElementsByClassName("enlace")[4] ', enlace .textContent );
-enlace .style .color = '#333333';
-enlace .innerText = "FAQ's";                     // Cambia 'Temas' por  FAQ's
-
-
+console .log( listaEnlaces );
