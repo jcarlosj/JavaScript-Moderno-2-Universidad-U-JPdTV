086a43bedfbc4e41747cb9bd98c613681df440ee
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-01-23 18:32:55 -0500

    Traversing en JavaScript (del Hijo al Padre)

--- js/app.js
+++ js/app.js
@@ -1,77 +1,32 @@
-/* Traversing en JavaScript (del Padre al Hijo) */
-const navegacion = document .querySelector( '#principal' );
+/* Traversing en JavaScript (del Hijo al Padre) */
+const enlaces = document .querySelectorAll( '.enlace' );       
 
-console .log( navegacion .childNodes );     // childNodes: Nodos hijo ( 'a.enlace' y 'text' ) 
-                                            // los nodos 'text' son los saltos de línea en el código entre cada 'a'
+console .log( 'enlaces[0] ', enlaces[ 0 ] );                                // Obtiene el enlace 
+console .log( 'enlaces[0].parentNode ', enlaces[ 0 ] .parentNode );         // Obtiene el elemento padre del enlace
+console .log( 'enlaces[0].parentElement ', enlaces[ 0 ] .parentElement );   // Obtiene el elemento padre del enlace (Forma recomendada)
+console .log( 'enlaces[0].parentElement ', enlaces[ 0 ] .parentElement .parentNode );   // Obtiene el elemento padre del enlace
+console .log( 'enlaces[0].parentElement ', enlaces[ 0 ] .parentElement .parentElement );   // Obtiene el elemento padre del elemento padre del enlace
 
-console .log( navegacion .children );       // children: Nodos hijo ( 'a.enlace' ) devuelve una colección
-                                            // Elimina los saltos de línea en el código entre cada 'a'
+/* NOTA: Se puede ir del Hijo al Padre cuanto lo permita la estructura del DOM */ 
 
-/* Asignamos nuevo contenido al primer enlace hijo del menú principal y al tiempo lo imprimimos por consola */                                            
-console .log( navegacion .children[ 0 ] .textContent = 'Nuevo Enlace' );
-
-/* Obtener Nombre de nodo */                                            
-console .log( navegacion .nodeName );                   // NAV ---> (Referencia al tag 'nav') 
-console .log( navegacion .children[ 0 ] .nodeName );    // A   ---> (Referencia al tag 'a') 
-
-/* Obtener Tipo de Nodo 
-   Usualmente regresan el tipo en número 
-   Tipo 1: Elementos HTML
-   Tipo 2: Atributos
-   Tipo 3: Nodos de Texto
-   Tipo 8: Comentarios
-   Tipo 9: Documentos
-   Tipo 10: DOCTYPE 
-   */
-console .log( navegacion .nodeType );                   // 1
-console .log( navegacion .children[ 0 ] .nodeType );    // 1
-
-/* Barra */
-const barra = document .querySelector( '.barra' );
+/* Implementamos algo en la vista */
+const cursos = document .querySelectorAll( '.card' );
 
 console .log( 
-    'barra .children[ 0 ] ', 
-    barra .children[ 0 ]                                        // Accede al contenedor 'container'
-);                                   
+    'cursos[ 0 ] .parentElement .style .border = "3px solid red" ', 
+    cursos[ 0 ] .parentElement .style .border = '3px solid red'                                     // Enlace que rodea el Card      
+);                      
 console .log( 
-    'barra .children[ 0 ] .children[ 0 ]', 
-    barra .children[ 0 ] .children[ 0 ]                         // Accede a la fila 'row'
-);                    
+    'cursos[ 0 ] .parentElement .parentElement.style .border = "3px solid blue"', 
+    cursos[ 0 ] .parentElement .parentElement.style .border = '3px solid blue'                      // Primera Fila de Cards    
+);   
 console .log( 
-    'barra .children[ 0 ] .children[ 0 ] .children[ 0 ]', 
-    barra .children[ 0 ] .children[ 0 ] .children[ 0 ]          // Accede al icono 'four columns icono icono1'
-);     
+    'cursos[ 0 ] .parentElement .parentElement .parentElement.style .border = "3px solid green"', 
+    cursos[ 0 ] .parentElement .parentElement .parentElement.style .border = '3px solid green'      // Todos los Cards (includo el título) 
+);         
 
-/* Cards de Cursos */
-const cardsCursos = document .querySelectorAll( '.card' );
-
-console .log( 
-    'cardsCursos ',                     // 'NodeList' ---> Colección de elementos
-    cardsCursos 
-);
-
-/* Primer elemento Hijo */
-console .log( 
-    'cardsCursos[ 0 ] .firtsChild', 
-    cardsCursos[ 0 ] .firtsChild         // #text ---> Primer elemento hijo (Saltos de línea en el código)
-);                    
-console .log( 
-    'cardsCursos[ 0 ] .firtsElementChild', 
-    cardsCursos[ 0 ] .firtsElementChild  // Primer elemento hijo de 'cardsCursos'
-);
-
-/* Último elementos Hijo */
-console .log( 
-    'cardsCursos[ 0 ] .lastChild', 
-    cardsCursos[ 0 ] .lastChild         // #text ---> Último elemento hijo (Saltos de línea en el código)
-);                    
-console .log( 
-    'cardsCursos[ 0 ] .lastElementChild', 
-    cardsCursos[ 0 ] .lastElementChild  // Último elemento hijo de 'cardsCursos'
-);
-
-/* Contar cuantos elementos tiene una colección */
-console .log( 
-    'cardsCursos[ 0 ] .childElementCount', 
-    cardsCursos[ 0 ] .childElementCount
+/* Cambiar el titulo de los cursos */
+console .log(
+    'Cambia el título',
+    cursos[ 0 ] .parentElement .parentElement .parentElement .children[ 0 ] .textContent = 'Courses Online'
 );
