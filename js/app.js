a80b51e07dafa56401a59215a753adbe779e3033
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-01-23 16:26:14 -0500

    Selecciona múltiples elementos en JavaScript usando 'getElementsByTagName'
    Aclaración:
    Este método es para seleccionar por tag y retorna una colección
    de elementos

--- js/app.js
+++ js/app.js
@@ -1,15 +1,28 @@
-/* Seleccionando múltiples elementos de JavaScript (Mezclando métodos de selección) */
-const listaEnlaces = document .querySelector( '#principal' ) .getElementsByClassName( 'enlace' );           
+/* Seleccionando múltiples elementos de JavaScript */
+const listaEnlaces = document .getElementsByTagName( 'a' );     // Selecciona todos los elementos con el tag 'a' (en este caso)     
 let enlaces;
+
+/* ---- RECORRE Y CAMBIA ESTILOS COMO COLECCIÓN ---- */
+console .group( 'Colección de enlaces' );
+    for( let i = 3; i <= 14; i++ ) {
+        listaEnlaces[ i ] .style .background = '#F1F1F1';
+        listaEnlaces[ i ] .style .border = '#C1C1C1';
+        listaEnlaces[ i ] .innerText = 'AGREGAR';
+    }
+console .groupEnd();
 
 /* Convertir Colección a 'Array' */
 enlaces = Array .from( listaEnlaces );
 
-/* Recorre y cambia estilos de los elementos seleccionados */
-enlaces .forEach( function( elemento ) {
-    elemento .style .fontSize = '17px';
-    elemento .style .textDecoration = 'underline';
-    elemento .style .fontWeight = 'bold';
-});
+/* ---- RECORRE Y CAMBIA ESTILOS COMO ARRAY ---- */
+console .group( 'Array de enlaces' );
+    enlaces .forEach( function( elemento ) {
+        elemento .style .textTransform = 'uppercase';
+        elemento .style .fontSize = '18px';
+        elemento .style .color = 'green';
+        console .log( 'enlace:', elemento .innerText );
+    });
+console .groupEnd();
 
-console .log( listaEnlaces );
+console .log( 'listaEnlaces (Colección): ', listaEnlaces );
+console .log( 'enlaces (Array): ', enlaces );
