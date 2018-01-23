56da566dd8a082327c5e9ab8f61c687ff659d845
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-01-23 14:04:27 -0500

    Selecciona un elemento o todos usando 'querySelector' ó 'querySelectorAll'
    Aclaración:
    Esta forma de selección solo retorna un valor
     - En el caso de 'querySelector' solo retorna el elemento
     - En el caso de 'querySelectorAll' solo retorna un 'Array' con elementos

--- js/app.js
+++ js/app.js
@@ -1,19 +1,50 @@
-/* Seleccionando elementos y aplicandole propiedades */
-let encabezado;
+/* Seleccionando un elemento de JavaScript (usando Query Selector) retornando un solo valor
+  -----------------------------------------------------------------------------------------
+   1. Permite que podamos seleccionar un elemento por ID o por CLASS 
+   2. Cuando selecionamos una clase que está en varios elementos 'querySelector'
+      solo selecciona el primero que encuentre */
 
-/* Obtiene el elemento por su ID */
-encabezado = document .getElementById( 'encabezado' );  
+/* ---- SELECCIÓN UN ELEMENTO (Retorna un elemento) ---- */
 
-/* Imprime el contenido del elemento */
-console .log( 'Inicialmente: ', encabezado .textContent );      // 'Cursos en línea'
+/* Ejemplo 1: Cambiamos los estilos del elemento 'encabezado' */      
+const encabezado = document .querySelector( '#encabezado' );        // Usamos . o # según sea un ID o un CLASS 
 
-/* Cambia los estilos del elemento */
-encabezado .style .background = '#33C3F0';
-encabezado .style .color = '#FFF';
-encabezado .style .padding = '20px';
+encabezado .style .color = '#1EAEDB';
+encabezado .style .fontSize = '70px';
+encabezado .style .textTransform = 'lowercase';
+encabezado .style .borderBottom = '5px solid #1EAEDB';
 
-// Cambia el contenido del elemento (también se puede usar: textContent)
-encabezado .innerText = 'Los mejores cursos'; 
+console .log( encabezado );
 
-/* Imprime el contenido final del elemento */
-console .log( 'Finalmente: ',  encabezado .innerText );         // 'Los mejores cursos'
+// Ejemplo 2: Usando 'Nesting' propiedades de CSS 3.0 
+const primerEnlace     = document .querySelector( '#principal a:first-child' ),
+      enlaceEspecifico = document .querySelector( '#principal a:nth-child( 3 )' ),  // 'Aplicaciones Móviles'
+      ultimoEnlace     = document .querySelector( '#principal a:last-child' );
+
+console .log( 'Primer enlace ', primerEnlace );
+console .log( 'Enlace específico ', enlaceEspecifico );
+console .log( 'Último enlace ', ultimoEnlace );      
+
+/* ---- SELECCIÓN DE TODOS LOS ELEMENTOS CON LA MISMA CLASE (Retorna un Array de elementos) ---- */
+
+// Ejemplo 3: Cambiar los enlaces del Footer
+const enlaces = document .querySelectorAll( '.enlace' );
+
+enlaces .forEach( elemento => {
+    elemento .style .textTransform = 'uppercase';
+    elemento .style .fontWeight = 'bold';    
+});
+
+console .log( 'Enlaces ', enlaces );
+
+// Ejemplo 4: Cambiar los enlaces del botón [Agregar al Carrito]
+const enlacesAgregarAlCarrito = document .querySelectorAll( 'div.card a.agregar-carrito' );
+
+enlacesAgregarAlCarrito .forEach( function( elemento ) {
+    elemento .textContent = 'Agregar';
+    elemento .style .fontSize = '22px';
+});
+
+console .log( 'Enlaces ', enlacesAgregarAlCarrito );
+
+/* NOTA: 'Nesting' Es seleccionar elementos padre y luego elementos hijo */
