5096c6c12112011cdbb60d6ea089b7c8832a3b28
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-01-23 16:45:31 -0500

    Selecciona múltiples elementos en JavaScript usando 'querySelectorAll', Nesting, CSS 3.0

--- js/app.js
+++ js/app.js
@@ -1,28 +1,10 @@
 /* Seleccionando múltiples elementos de JavaScript */
-const listaEnlaces = document .getElementsByTagName( 'a' );     // Selecciona todos los elementos con el tag 'a' (en este caso)     
-let enlaces;
+const enlaces = document .querySelectorAll( '#principal a.enlace:nth-child( odd )' );       // Seleccionamos los enlaces impares del menú principal del footer
 
-/* ---- RECORRE Y CAMBIA ESTILOS COMO COLECCIÓN ---- */
-console .group( 'Colección de enlaces' );
-    for( let i = 3; i <= 14; i++ ) {
-        listaEnlaces[ i ] .style .background = '#F1F1F1';
-        listaEnlaces[ i ] .style .border = '#C1C1C1';
-        listaEnlaces[ i ] .innerText = 'AGREGAR';
-    }
-console .groupEnd();
+enlaces .forEach( function( impares ) {
+    impares .style .background = '#1EAEDB';
+    impares .style .color = 'white';
+    impares .style .fontWeight = 'bold';
+});
 
-/* Convertir Colección a 'Array' */
-enlaces = Array .from( listaEnlaces );
-
-/* ---- RECORRE Y CAMBIA ESTILOS COMO ARRAY ---- */
-console .group( 'Array de enlaces' );
-    enlaces .forEach( function( elemento ) {
-        elemento .style .textTransform = 'uppercase';
-        elemento .style .fontSize = '18px';
-        elemento .style .color = 'green';
-        console .log( 'enlace:', elemento .innerText );
-    });
-console .groupEnd();
-
-console .log( 'listaEnlaces (Colección): ', listaEnlaces );
-console .log( 'enlaces (Array): ', enlaces );
+console .log( enlaces );
