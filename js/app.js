5f6cc09a7558f1f46ca765d551ca526b21d71423
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-01-24 12:53:32 -0500

    Clonar un elemento en JavaScript
    Aclaración
     1. Es importante seleccionar el elemento (o nodo) a clonar
     2. Clonar el elemento
     3. Seleccionar el elemento PADRE donde se va a agregar en el DOM
     4. Agregar el elemento clonado
    
    Métodos:
     - 'cloneNode': Este método permite clonar un elemento, recibe un
                    parámetro booleano
    
       true: le indica a 'cloneNode' que se desea clonar el elemento con
             todos sus elementos hijos
       false: le indica a 'cloneNode' que se desea clonar exclusivamente
             el elemento, sin tener consideración por sus nodos hijos
                    de los elementos

--- js/app.js
+++ js/app.js
@@ -1,59 +1,23 @@
-/* Agregar/Eliminar atributo de un elemento con JavaScript */
+/* Clonar un elemento con JavaScript */
 const primerEnlace = document .querySelector( '.enlace' );          // Primer enlace menú principal
 
-let atributo;
+let elementoClonado, elementoPadre;
 
 console .group( 'Menú principal ' );
     console .log( 'Primer enlace ', primerEnlace );
 
-    console .group( 'Atributo "href" inicial del elemento ' );
-        
-        /* Obtiene atributo 'href' del elemento */
-        atributo = primerEnlace .getAttribute( 'href' );    
-        console .log( 'Al aplicar "getAttribute" ', atributo );
-
-    console .groupEnd();
-
-    console .group( 'Modificar el atributo "href" del elemento ' );
-        
-        /* Obtiene atributo 'href' del elemento */
-        primerEnlace .setAttribute( 'href', 'http://twitter.com' );    
-        atributo = primerEnlace;    // Hay que hacerlo para que funcione
-        console .log( 'Luego de aplicar el "setAttribute" ', atributo );
-
-    console .groupEnd();
-
-    console .group( 'Crear atributos personalizados al elemento (HTML5) ' );
-        
-        /* Obtiene atributo 'data-id' del elemento */
-        primerEnlace .setAttribute( 'data-id', 20 );    
-        atributo = primerEnlace;    // Hay que hacerlo para que funcione
-        console .log( 'Luego de aplicar el "setAttribute" ', atributo );
-
-    console .groupEnd();
-
-    console .group( 'Verificar que un atributo existe en un elemento ' );
-        
-        /* Verifica atributo 'data-id' del elemento */
-        atributo = primerEnlace .hasAttribute( 'data-id' );    
-        console .log( 'Luego de aplicar el "hasAttribute" ', atributo );        // true
-        atributo = primerEnlace .hasAttribute( 'otro-atributo' );       
-        console .log( 'Luego de aplicar el "hasAttribute" ', atributo );        // false
-
-    console .groupEnd();
-
-    console .group( 'Eliminar atributos del elemento (HTML5) ' );
-        
-        /* Elimina atributo 'data-id' del elemento */
-        primerEnlace .removeAttribute( 'data-id' );    
-        atributo = primerEnlace;    // Hay que hacerlo para que funcione
-        console .log( 'Elimina data-id "removeAttribute" ', atributo );
-
-        /* Elimina atributo 'id' del elemento */
-        primerEnlace .removeAttribute( 'id' );    
-        atributo = primerEnlace;    // Hay que hacerlo para que funcione
-        console .log( 'Elimina id "removeAttribute" ', atributo );
-
-    console .groupEnd();
+    /* Clonamos un enlace */
+    elementoClonado = primerEnlace .cloneNode( true );    // true: Los hijos del nodo deben ser copiados 
+                                                          //       (en este caso 'true' por que hay que 
+                                                          //       copiar el nodo del 'contenido' del enlace)
+                                                          // false: Los hijos del nodo no deben ser copiados
+    console .log( 'elementoClonado ', elementoClonado );
+    
+    /* Accedemos al elemento Padre */
+    elementoPadre = primerEnlace .parentElement;
+    console .log( 'elementoPadre ', elementoPadre );
+    
+    /* Agregamos 2 veces el elemento clonado */
+    elementoPadre .appendChild( elementoClonado );      'Para Tu Negocio'
 
 console .groupEnd();
