7276a4244579bc85f369fa9b85395ea7587ed0c3
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-01-24 18:58:00 -0500

    Eventos para los elementos INPUT 'keyup' en JavaScript

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
-/* Eventos 'keydown' en JavaScript 
-   Registra cada vez que presionamos una tecla */
+/* Eventos para los elementos INPUT 'keyup' en JavaScript 
+   Registra cada vez que se suelta una tecla */
 const buscador   = document .querySelector( '#buscador' );          // Campo del buscador
 
 /* Implementa el Evento 'keydown' en el input de búsqueda */
-buscador .addEventListener( 'keydown', obtenerEvento );
+buscador .addEventListener( 'keyup', obtenerEvento );
 
 /* Creamos una función que capture el evento */
 function obtenerEvento( evento ) {
