14422dab9f86d9adf141d18094c392ac37fb9969
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-01-24 11:56:09 -0500

    Agregar/Eliminar atributos de un elemento en JavaScript
    Aclaración
     1. Es importante seleccionar el elemento (o nodo)
     2. Aplicar el método de acuerdo a la acción que se desee
    
    Métodos:
     - 'getAttribute': Obtiene el valor del atributo seleccionado del elemento
     - 'setAttribute': Cambia el valor del atributo seleccionado del elemento
                       o Crea un atributo nuevo con su respectivo valor
     - 'hasAttribute': Verifica si el atributo seleccionado existe retorna
                       [true/false]
     - 'removeAttribute': Elimina el atributo seleccionado del elemento

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-/* Agregar/Eliminar clases de los elementos con JavaScript */
+/* Agregar/Eliminar atributo de un elemento con JavaScript */
 const primerEnlace = document .querySelector( '.enlace' );          // Primer enlace menú principal
 
 let atributo;
@@ -6,40 +6,54 @@ let atributo;
 console .group( 'Menú principal ' );
     console .log( 'Primer enlace ', primerEnlace );
 
-    console .group( 'ID y Clases iniciales del elemento ' );
+    console .group( 'Atributo "href" inicial del elemento ' );
         
-        /* Obtenemos ID y Clases del elemento */
-        atributo = primerEnlace .id;           // Obtiene el id del elemento
-        console .log( 'id ', atributo );
-        atributo = primerEnlace .className;    // Obtiene la clase del elemento
-        console .log( 'className ', atributo );
-        atributo = primerEnlace .classList;    // Obtiene un 'DOMTokenList' del elemento
-        console .log( 'classList ', atributo );
+        /* Obtiene atributo 'href' del elemento */
+        atributo = primerEnlace .getAttribute( 'href' );    
+        console .log( 'Al aplicar "getAttribute" ', atributo );
 
     console .groupEnd();
 
-    console .group( 'Se agregó la clase nueva "nueva-clase" al elemento ' );
+    console .group( 'Modificar el atributo "href" del elemento ' );
         
-        atributo = primerEnlace .classList .add( 'nueva-clase-1' );   // Agrega la clase 'nueva-clase'
-
-        /* Obtenemos Clases del elemento */
-        atributo = primerEnlace .className;    // Obtiene la clase del elemento
-        console .log( 'className ', atributo );
-        atributo = primerEnlace .classList;    // Obtiene un 'DOMTokenList' del elemento
-        console .log( 'classList ', atributo );
+        /* Obtiene atributo 'href' del elemento */
+        primerEnlace .setAttribute( 'href', 'http://twitter.com' );    
+        atributo = primerEnlace;    // Hay que hacerlo para que funcione
+        console .log( 'Luego de aplicar el "setAttribute" ', atributo );
 
     console .groupEnd();
 
-    console .group( 'Se elimina la clase "enlace" al elemento ' );
+    console .group( 'Crear atributos personalizados al elemento (HTML5) ' );
         
-    atributo = primerEnlace .classList .remove( 'enlace' );   // Elimina la clase 'enlace' 
+        /* Obtiene atributo 'data-id' del elemento */
+        primerEnlace .setAttribute( 'data-id', 20 );    
+        atributo = primerEnlace;    // Hay que hacerlo para que funcione
+        console .log( 'Luego de aplicar el "setAttribute" ', atributo );
 
-    /* Obtenemos Clases del elemento */
-    atributo = primerEnlace .className;    // Obtiene la clase del elemento
-    console .log( 'className ', atributo );
-    atributo = primerEnlace .classList;    // Obtiene un 'DOMTokenList' del elemento
-    console .log( 'classList ', atributo );
+    console .groupEnd();
 
-console .groupEnd();
+    console .group( 'Verificar que un atributo existe en un elemento ' );
+        
+        /* Verifica atributo 'data-id' del elemento */
+        atributo = primerEnlace .hasAttribute( 'data-id' );    
+        console .log( 'Luego de aplicar el "hasAttribute" ', atributo );        // true
+        atributo = primerEnlace .hasAttribute( 'otro-atributo' );       
+        console .log( 'Luego de aplicar el "hasAttribute" ', atributo );        // false
+
+    console .groupEnd();
+
+    console .group( 'Eliminar atributos del elemento (HTML5) ' );
+        
+        /* Elimina atributo 'data-id' del elemento */
+        primerEnlace .removeAttribute( 'data-id' );    
+        atributo = primerEnlace;    // Hay que hacerlo para que funcione
+        console .log( 'Elimina data-id "removeAttribute" ', atributo );
+
+        /* Elimina atributo 'id' del elemento */
+        primerEnlace .removeAttribute( 'id' );    
+        atributo = primerEnlace;    // Hay que hacerlo para que funcione
+        console .log( 'Elimina id "removeAttribute" ', atributo );
+
+    console .groupEnd();
 
 console .groupEnd();
