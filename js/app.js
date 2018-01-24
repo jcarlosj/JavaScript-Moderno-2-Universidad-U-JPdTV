c9dd7c628aef833054e7adf21f8d13b6bca8391d
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-01-24 10:26:25 -0500

    Eliminar elementos en JavaScript
    Aclaración
    Existen dos métodos en JavaScript para eliminar elementos
    
     - 'remove': para eliminar un elemento desde el mismo elemento seleccionado
     - 'removeChild': para eliminar un elemento desde el elemento padre

--- js/app.js
+++ js/app.js
@@ -1,21 +1,11 @@
-/* Reemplazando elementos con JavaScript */
-const nuevoEncabezado = document .createElement( 'h2' );    // Crea el elemento 'h2'
+/* Eliminar elementos con JavaScript */
+const enlaces = document .querySelectorAll( '.enlace' );    // Enlaces del footer
+      navegacion = document .querySelector( '#principal' ); // '#principal' ---> Toda la estructura que referencia este ID   
 
-/* Agrega propiedades al elemento */
-nuevoEncabezado .id = 'encabezado';                                                                // Agrega un id al elemento 'h2'
-nuevoEncabezado .appendChild( document .createTextNode( 'Los mejores cursos del 2018' ) );         // Agrega el texto al elemento 'h2'
+console .log( enlaces );
 
-console .log( 'Nuevo encabezado: ', nuevoEncabezado );
+/* Eliminar elemento (desde el mismo elemento) */
+enlaces[ 0 ] .remove();                     // 'Para Tu Negocio' ---> Se elimina a si mismo 
 
-/* Seleccionamos el nodo (o elemento) que vamos a cambiar */
-const encabezadoAnterior = document .querySelector( '#encabezado' );
-console .log( 'Encabezado anterior ', encabezadoAnterior );
-
-/* Nos posicionamos en el nodo padre */
-const elementoPadre = encabezadoAnterior .parentElement;                // 'lista-cursos'
-console .log( 'Padre de "encabezado anterior" ', elementoPadre );
-
-/* Reemplazamos el encabezado */
-elementoPadre .replaceChild( nuevoEncabezado, encabezadoAnterior );
-
-// Reemplaza el elemento anterior por el nuevo que hemos creado
+/* Eliminar elemento desde su elemento Padre */
+navegacion .removeChild( enlaces[ 2 ] );    // 'Aplicaciones Móviles'
