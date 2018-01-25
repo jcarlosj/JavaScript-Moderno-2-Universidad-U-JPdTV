87199255464c350f44d67603c9a192c2766177ad
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-01-24 19:16:11 -0500

    Eventos para los elementos INPUT 'copy' en JavaScript

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
-/* Eventos para los elementos INPUT 'cut' en JavaScript 
-   Cuando cortamos contenido que se encuentra dentro del input ya sea 
-   a través del shortcut (Ctrl+X) o a través del menú contextual */
+/* Eventos para los elementos INPUT 'copy' en JavaScript 
+   Cuando copiamos contenido que se encuentra dentro del input ya sea 
+   a través del shortcut (Ctrl+C) o a través del menú contextual */
 const buscador   = document .querySelector( '#buscador' );          // Campo del buscador
 
-/* Implementa el Evento 'cut' en el input de búsqueda */
-buscador .addEventListener( 'cut', obtenerEvento );
+/* Implementa el Evento 'copy' en el input de búsqueda */
+buscador .addEventListener( 'copy', obtenerEvento );
 
 /* Creamos una función que capture el evento */
 function obtenerEvento( evento ) {
