931e2c301decdd6e565e7c8dd90b355b33b116de
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-01-24 19:06:50 -0500

    Eventos para los elementos INPUT 'focus' en JavaScript

--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
-/* Eventos para los elementos INPUT 'keypress' en JavaScript 
-   Registra cada vez que se presiona una tecla (Similar a 'keydown') */
+/* Eventos para los elementos INPUT 'focus' en JavaScript 
+   Registra cada que damos click dentro del input y nos posicionamos dentro del input
+   se dice entonces que tenemos el foco */
 const buscador   = document .querySelector( '#buscador' );          // Campo del buscador
 
-/* Implementa el Evento 'keypress' en el input de búsqueda */
-buscador .addEventListener( 'keypress', obtenerEvento );
+/* Implementa el Evento 'focus' en el input de búsqueda */
+buscador .addEventListener( 'focus', obtenerEvento );
 
 /* Creamos una función que capture el evento */
 function obtenerEvento( evento ) {
-    document .querySelector( '#encabezado' ) .innerText = buscador .value;
+    //document .querySelector( '#encabezado' ) .innerText = buscador .value;
     console .log( buscador .value );
     console .log( `evento.type: ${ evento .type } ` );
 }      
