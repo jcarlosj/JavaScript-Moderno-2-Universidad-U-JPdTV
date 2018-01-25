6e8926cf9afc387f333893fce07d322c33276911
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-01-24 19:14:07 -0500

    Eventos para los elementos INPUT 'cut' en JavaScript

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
-/* Eventos para los elementos INPUT 'blur' en JavaScript 
-   Opuesto al 'focus' Registra cada que damos click fuera del input y nos posicionamos fuera del input
-   se dice entonces que perdimos el foco */
+/* Eventos para los elementos INPUT 'cut' en JavaScript 
+   Cuando cortamos contenido que se encuentra dentro del input ya sea 
+   a través del shortcut (Ctrl+X) o a través del menú contextual */
 const buscador   = document .querySelector( '#buscador' );          // Campo del buscador
 
-/* Implementa el Evento 'blur' en el input de búsqueda */
-buscador .addEventListener( 'blur', obtenerEvento );
+/* Implementa el Evento 'cut' en el input de búsqueda */
+buscador .addEventListener( 'cut', obtenerEvento );
 
 /* Creamos una función que capture el evento */
 function obtenerEvento( evento ) {
