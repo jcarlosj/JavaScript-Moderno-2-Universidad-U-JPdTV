5579a104bdbeece6ec7cf24cf2f0c9241eff2728
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-01-25 11:28:34 -0500

    LocalStorage y Session Storage  en JavaScript (CRUD)

--- js/app.js
+++ js/app.js
@@ -1,36 +1,44 @@
-/* Delegation: es una forma en la que se puede esperar un click en cualquier lugar
-               comprobar que elemento esta haciendo click. 
-               
-    1. Representa otra forma diferente de evitar el Event Bubbling (Burbuja de Eventos)
-    2. Es una alternativa para no crear un Event Listener por cada elemento al
-       que se desea agregar un evento */
+/* Local Storage y Session Storage 
+   Local Storage: Permite agregar cierta información en el navegador */
 
-/* Escuchar todos los eventos ocurridos en el cuerpo del documento */
-document .body .addEventListener( 'click', clickEnElemento );
+/* ---- LOCAL STORAGE ---- */
 
-function clickEnElemento( evento ) {
-    evento .preventDefault();       // Evitar cualquier cosa que haga el navegador con el evento por defecto
+// Agregar al Local Storage (Se borra todo desde la herramienta de desarrollo del navegador o si decide limpiar todo el cache de la computadora)
+localStorage .setItem( 'nombre', 'Juan' );              // Asigna un valor al Local Storage
+localStorage .setItem( 'apellido', 'Jiménez' );         // Asigna un valor al Local Storage
 
-    console .group( 'Click!' );     
-        console .log( 'evento .target .classList', evento .target .classList );     // Obtiene 'DOMTokenList' de las clases del elemento
-    console .groupEnd();     
+let nombre = localStorage .getItem( 'nombre' ),         // Obtiene un valor del LocalStorage
+      apellido = localStorage .getItem( 'apellido' );   // Obtiene un valor del LocalStorage
 
-    /* Eliminar Curso */
-    if( evento .target .classList .contains( 'borrar-curso' ) ) {               // 'contains: se va a encargar de verificar que algunas de las clases que se le indican esten contenidas'
-        console .group( 'Contiene la clase "borrar-curso"?', 'Sí!' );
-            console .log( 'elemento: ', evento .target .parentElement );        // Elemento Padre de la clase 'borrar-curso'                       
-            evento .target .parentElement .parentElement .remove();             // Eliminar elemento 'tr' ( Padre del padre de la clase )
-        console .groupEnd();
+console .group( 'LocalStorage' );
+    console .log( 'LocalStorage: ', localStorage );
+    console .log( `${ nombre } ${ apellido }`)
+console .groupEnd();
+
+// Eliminar del Local Storage
+localStorage .removeItem( 'apellido' );                 // Obtiene elimina un valor del LocalStorage
+apellido = localStorage .getItem( 'apellido' );
+
+console .group( 'LocalStorage: Después de eliminar apellido' );
+    
+    console .log( 'LocalStorage: ', localStorage );     // Muestra LocalStorage
+    
+    if( apellido ) {
+        console .log( `${ nombre } ${ apellido }`);
     }
     else {
-        console .group( 'Contiene la clase "borrar-curso"?', 'No!' );
-            console .log( 'Intenta con otro elemento' );        
-        console .groupEnd();
+        console .log( `${ nombre } `);
     }
+    
+console .groupEnd();
 
-    /* Agregar Curso */
-    if( evento .target .classList .contains( 'agregar-carrito' ) ) {            // 'contains: se va a encargar de verificar que algunas de las clases que se le indican esten contenidas'
-        console .log( 'Curso Agregado' );
-    }
+// Limpia Local Storage
+console .group( 'Limpiar LocalStorage: ' );
+    localStorage .clear();                              // Limpia el LocalStorage
+    console .log( 'LocalStorage: ', localStorage );     // Muestra LocalStorage
+console .groupEnd();    
+    
+/* ---- SESSION STORAGE ---- */
 
-}
+// Agregar al Session Storage (Se borra todo si se cierra el navegador)
+sessionStorage .setItem( 'nombre', 'Carlos' );
