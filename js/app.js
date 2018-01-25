28de9316b0d94e983ae491133a8a875c52580261
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-01-25 10:30:37 -0500

    Delegation en JavaScript (evitar Event Bubbling y Event Listener)

--- js/app.js
+++ js/app.js
@@ -1,21 +1,36 @@
-/* Detener Event Bubbling (Burbuja de Eventos) */
+/* Delegation: es una forma en la que se puede esperar un click en cualquier lugar
+               comprobar que elemento esta haciendo click. 
+               
+    1. Representa otra forma diferente de evitar el Event Bubbling (Burbuja de Eventos)
+    2. Es una alternativa para no crear un Event Listener por cada elemento al
+       que se desea agregar un evento */
 
-const card = document .querySelector( '.card' ),                            // Selecciona elemento primera tarjeta
-      infoCard = document .querySelector( '.info-card' ),                   // Selecciona elemento información de la primera tarjeta
-      botonAgregarCarrito = document .querySelector( '.agregar-carrito' );  // Selecciona elemento del botón de la primera tarjeta 
+/* Escuchar todos los eventos ocurridos en el cuerpo del documento */
+document .body .addEventListener( 'click', clickEnElemento );
 
-/* El 'Event Bubbling' es la propagación de eventos en elementos que se contienen los unos a los otros */
-card .addEventListener( 'click', function( evento ) {                   // Dispara el evento para: card  
-    console .log( 'Click en Card' );
-    evento .stopPropagation();          // Evita la propagación de eventos
-});
+function clickEnElemento( evento ) {
+    evento .preventDefault();       // Evitar cualquier cosa que haga el navegador con el evento por defecto
 
-infoCard .addEventListener( 'click', function(  evento ) {               // Dispara el evento para: infoCard
-    console .log( 'Click en Información del Curso' );           
-    evento .stopPropagation();          // Evita la propagación de eventos
-});
+    console .group( 'Click!' );     
+        console .log( 'evento .target .classList', evento .target .classList );     // Obtiene 'DOMTokenList' de las clases del elemento
+    console .groupEnd();     
 
-botonAgregarCarrito .addEventListener( 'click', function(  evento ) {    // Dispara el evento para: botonAgregarCarrito
-    console .log( 'Click en Botón Agregar al Carrito' );        
-    evento .stopPropagation();          // Evita la propagación de eventos
-});
+    /* Eliminar Curso */
+    if( evento .target .classList .contains( 'borrar-curso' ) ) {               // 'contains: se va a encargar de verificar que algunas de las clases que se le indican esten contenidas'
+        console .group( 'Contiene la clase "borrar-curso"?', 'Sí!' );
+            console .log( 'elemento: ', evento .target .parentElement );        // Elemento Padre de la clase 'borrar-curso'                       
+            evento .target .parentElement .parentElement .remove();             // Eliminar elemento 'tr' ( Padre del padre de la clase )
+        console .groupEnd();
+    }
+    else {
+        console .group( 'Contiene la clase "borrar-curso"?', 'No!' );
+            console .log( 'Intenta con otro elemento' );        
+        console .groupEnd();
+    }
+
+    /* Agregar Curso */
+    if( evento .target .classList .contains( 'agregar-carrito' ) ) {            // 'contains: se va a encargar de verificar que algunas de las clases que se le indican esten contenidas'
+        console .log( 'Curso Agregado' );
+    }
+
+}
